dea16d74c2909334d16465dec08f3917d0f9790a
Manabu <manabu@Manabus-MacBook-Air.local>
2026-02-06 12:10:09 +0700

    Update CSS link to root-relative path in header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>manablog</title>
 	<link rel="stylesheet" href="https://use.typekit.net/jkb4xph.css">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="/style.css">
 	<link rel="shortcut icon" href="https://manablog.org/wp-content/themes/manabu/images/favicon.ico">
 </head>
 
